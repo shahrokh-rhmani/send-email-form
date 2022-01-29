bad36d3ecea12c7bb7e7d7b2d4706c48d92c7425
Ali <shahrokh.r1379@gmail.com>
2022-01-29 15:46:20 +0330

    views.py mail

--- root/mail/views.py
+++ root/mail/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .forms import EmailForm
+from django.core.mail import send_mail
+from django.conf import settings
 
-# Create your views here.
+def sendmail(request):
+    message_sent = False
+
+    if request.method == 'POST':
+        form = EmailForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            subject = 'send an email'
+            message = cd['message']
+            send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [cd['recipient']])
+
+            message_sent = True
+
+    else:
+        form = EmailForm()
+
+    context = {
+        'form': form,
+        'messageSent':messageSent,
+    }
+
+    return render(request, 'index.html', context)
